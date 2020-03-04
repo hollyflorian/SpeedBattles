5fcfa12ab245d21f4c53c24459b0cdb436284ead
florianholly <florian.holly@towa.at>
2020-03-04 14:43:47 +0100

    Add Basic Materials and Movement Controller

---
+++ New Unity Project/Assets/Scripts/Player_Look.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Look : MonoBehaviour
+{
+    public float mouseSensitivity = 300f;
+    public Transform playerBody;
+    float rotation_X = 0f;
+
+    void Start()
+    {
+        // Hide and Lock Cursor
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        rotation_X -= mouseY;
+        rotation_X = Mathf.Clamp(rotation_X, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(rotation_X, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ New Unity Project/Assets/Scripts/Player_Movement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    public float playerSpeed = 10f;
+    public float playerGravity = -9f;
+    public float playerJump = 2f;
+    public CharacterController controller;
+
+    // GroundCheck
+    public Transform GroundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(GroundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0){
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move* playerSpeed * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && isGrounded){
+            velocity.y = Mathf.Sqrt(playerJump * -4f *playerGravity);
+        }
+
+        velocity.y += playerGravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
